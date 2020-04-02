922cd25a1f2e4fcbc9415e2d5ef60803a3d7110a
Mary Kay <maryrill@gmail.com>
2020-04-02 14:34:06 -0400

    Updates from afternoon, day 3

--- LibraryApi/Controllers/ReservationsController.cs
+++ LibraryApi/Controllers/ReservationsController.cs
@@ -7,25 +7,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace LibraryApi.Controllers
 {
     public class ReservationsController : Controller
     {
         LibraryDataContext Context;
         IWriteToTheReservationQueue ReservationQueue;
-
         public ReservationsController(LibraryDataContext context, IWriteToTheReservationQueue reservationQueue)
         {
             Context = context;
             ReservationQueue = reservationQueue;
         }
-
         [HttpPost("/reservations")]
-        public async Task<ActionResult> AddReservation([FromBody] PostReservationRequest request)
+        public async Task<ActionResult<Reservation>> AddReservation([FromBody] PostReservationRequest request)
         {
-            //validate model
-            //add to database
+            // validate
+            // add it to the database
             var reservation = new Reservation
             {
                 For = request.For,
@@ -35,15 +32,47 @@ namespace LibraryApi.Controllers
             };
             Context.Reservations.Add(reservation);
             await Context.SaveChangesAsync();
-
-            //write message to queue (RabbitMQ)
+            // write a message to the queue
+            // TODO: RabbitMQ
+            // Write the code you wish you had.
             await ReservationQueue.Write(reservation);
-
-
-            // return response
+            // return a response (201)
             return Ok(reservation);
         }
-
+        [HttpPost("/reservations/approved")]
+        public async Task<ActionResult> ApprovedReservation([FromBody] Reservation reservation)
+        {
+            var storedReservation = await Context.Reservations.SingleOrDefaultAsync(r => r.Id == reservation.Id);
+            if (storedReservation == null)
+            {
+                return BadRequest("No Pending Reservation with that id");
+                // decision time!
+            }
+            else
+            {
+                storedReservation.Status = ReservationStatus.Approved;
+                // do other stuff, or write a message to the queue that other processes will handle.
+                await Context.SaveChangesAsync();
+                return Accepted();
+            }
+        }
+        [HttpPost("/reservations/cancelled")]
+        public async Task<ActionResult> CancelledReservation([FromBody] Reservation reservation)
+        {
+            var storedReservation = await Context.Reservations.SingleOrDefaultAsync(r => r.Id == reservation.Id);
+            if (storedReservation == null)
+            {
+                return BadRequest("No Pending Reservation with that id");
+                // decision time!
+            }
+            else
+            {
+                storedReservation.Status = ReservationStatus.Cancelled;
+                // do other stuff, or write a message to the queue that other processes will handle.
+                await Context.SaveChangesAsync();
+                return Accepted();
+            }
+        }
         [HttpGet("/reservations/approved")]
         public async Task<ActionResult> GetApprovedReservations()
         {
@@ -53,9 +82,8 @@ namespace LibraryApi.Controllers
             // TODO: Project these into models. This is not a great way to do it. Classroom only.
             return Ok(response);
         }
-
         [HttpGet("/reservations/cancelled")]
-        public async Task<ActionResult> GetCancelledReservations()
+        public async Task<ActionResult> GetCancelledREservations()
         {
             var response = await Context.Reservations
             .Where(r => r.Status == ReservationStatus.Cancelled)
@@ -63,9 +91,8 @@ namespace LibraryApi.Controllers
             // TODO: Project these into models. This is not a great way to do it. Classroom only.
             return Ok(response);
         }
-
         [HttpGet("/reservations/pending")]
-        public async Task<ActionResult> GetPendingReservations()
+        public async Task<ActionResult> GetPendingREservations()
         {
             var response = await Context.Reservations
             .Where(r => r.Status == ReservationStatus.Pending)
